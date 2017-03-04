96892ee17c4332367e9dfb5e84ef1bd072abbe61
Maksim Kostromin <daggerok@gmail.com>
2017-03-04 14:49:32 +0200

    fix react vendors filename

--- query/tweet/react/config/webpack/plugins/extract-text-webpack-plugin.config.babel.js
+++ query/tweet/react/config/webpack/plugins/extract-text-webpack-plugin.config.babel.js
@@ -2,7 +2,7 @@ import { publicPath } from '../utils.babel';
 import { suffix } from '../output.config.babel';
 
 export default env => ({
-  filename: 'react-[name].css?' + suffix,
+  filename: '[name].css?' + suffix,
   disable: false,
   allChunks: true,
   publicPath,
